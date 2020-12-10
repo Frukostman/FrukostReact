47d834c4a6664d19b1275b2dfeed82c332026f0f
Santiago Homps <santiagohomps@gmail.com>
2020-12-09 22:34:03 -0300

    correccionlinkhome

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,13 +1,13 @@
 import './index.css';
 import useAppContext from '../../context/useAppContext';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
  
 
 export default function Cart() {
 
-    const funcion = () => {
-        console.log(carrito)
-    }
+    // const funcion = () => {
+    //     alert("hola")
+    // }
 
     const { carrito } = useAppContext()
     
@@ -19,7 +19,12 @@ export default function Cart() {
 
                 <> {carrito.length==0 ? 
                 
-                (<button><Link to={`/home`} onClick={() => funcion}/> volver a la home </button>) 
+                (<div>
+                    <p>No tenes productos en el carrito</p>
+                    <NavLink to={`/home`}>  Seguir comprando... </NavLink>
+                        
+                        
+                    </div>)
                 
                 : (<p>
                      {carrito.map((entry) => (
